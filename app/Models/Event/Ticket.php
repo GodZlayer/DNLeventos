31562850a7985806cd3e1970f4428175b8f848d3
GodZlayer <santoss.cog@gmail.com>
2024-10-15 01:18:59 -0300

    Registro Organizador removido por segurança
    Evitando scam e bot

--- app/Models/Event/Ticket.php
+++ app/Models/Event/Ticket.php
@@ -8,32 +8,38 @@ use App\Models\Event\TicketVariation;
 
 class Ticket extends Model
 {
-  use HasFactory;
+    use HasFactory;
 
-  protected $fillable = [
-    'event_id',
-    'event_type',
-    'title',
-    'ticket_available_type',
-    'ticket_available',
-    'max_ticket_buy_type',
-    'max_buy_ticket',
-    'description',
-    'pricing_type',
-    'price',
-    'f_price',
-    'early_bird_discount_type',
-    'early_bird_discount',
-    'early_bird_discount_amount',
+    protected $fillable = [
+        'event_id',
+        'event_type',
+        'title',
+        'ticket_available_type',
+        'ticket_available',
+        'max_ticket_buy_type',
+        'max_buy_ticket',
+        'description',
+        'pricing_type',
+        'price',
+        'f_price',
+        'early_bird_discount_type',
+        'early_bird_discount',
+        'early_bird_discount_amount',
+        'early_bird_discount_date',
+        'early_bird_discount_time',
+        'variations',
+        'trans_vars'
+    ];
 
-    'early_bird_discount_date',
-    'early_bird_discount_time',
-    'variations',
-    'trans_vars'
-  ];
-  //ticket_variations
-  public function ticket_variations()
-  {
-    return $this->hasMany(TicketVariation::class);
-  }
+    //ticket_variations
+    public function ticket_variations()
+    {
+        return $this->hasMany(TicketVariation::class);
+    }
+
+    //invoice relationship
+    public function invoice()
+    {
+        return $this->hasOne(Invoice::class);
+    }
 }
